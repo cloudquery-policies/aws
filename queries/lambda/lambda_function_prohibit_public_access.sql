a4ef6db682b633e6eb19b826f998ee8ff9869dd7
Itay Zagron <zagron@gmail.com>
2021-11-22 13:15:09 +0100

    Added lambda section

---
+++ queries/lambda/lambda_function_prohibit_public_access.sql
@@ -0,0 +1,13 @@
+SELECT account_id,
+       region,
+       arn
+FROM aws_lambda_functions,
+     jsonb_array_elements(policy_document -> 'Statement') AS statment
+WHERE statment->> 'Effect' = 'Allow'
+  AND (
+    statment->> 'Principal' = '*'
+   OR
+    statment-> 'Principal' ->> 'AWS' = '*'
+    )
+
+GROUP BY arn;
