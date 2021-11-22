d176fa902e869f8cb8bb4fe52961a1df1ff6bd9e
bbernays <ben@bernays.me>
2021-11-22 11:13:09 -0500

    Update publicly_writable_buckets.sql

--- queries/s3/publicly_writable_buckets.sql
+++ queries/s3/publicly_writable_buckets.sql
@@ -1,8 +1,13 @@
 select
     aws_s3_buckets.arn
 from
-    aws_s3_buckets
-    left join aws_s3_bucket_grants on aws_s3_buckets.cq_id = aws_s3_bucket_grants.bucket_cq_id
+    aws_s3_buckets -- Find and join all bucket ACLS that givea public write access
+    left join aws_s3_bucket_grants on aws_s3_buckets.cq_id = aws_s3_bucket_grants.bucket_cq_id 
+    -- Find all statements that could give public allow access 
+    -- Statements that give public access have 1) Effect == Allow 2) One of the following principal:
+    --       Principal = {"AWS": "*"}
+    --       Principal = {"AWS": ["arn:aws:iam::12345678910:root", "*"]}
+    --       Principal = "*"
     left join (
         select
             arn,
@@ -17,22 +22,26 @@ from
                     jsonb_array_elements(policy -> 'Statement') as statements
                 where
                     statements -> 'Effect' = '"Allow"'
-            ) as foo,
-            json_each_text(principals :: json) as principal_names
+            ) as foo        
         where
-            key = 'AWS'
-            and (
-                value = '*'
-                or value like '%"*"%'
+            principals = '"*"'
+            or (
+                principals :: jsonb ? 'AWS'
+                and (
+                    principals -> 'AWS' = '"*"'
+                    or principals -> 'AWS' @> '"*"'
+                )
             )
         group by
             arn
     ) as policy_allow_public on aws_s3_buckets.cq_id = aws_s3_bucket_grants.bucket_cq_id
 where
-    aws_s3_buckets.block_public_acls != true
-    and (
-        uri = 'http://acs.amazonaws.com/groups/global/AllUsers'
-        and permission in ('WRITE_ACP', 'FULL_CONTROL')
+    (
+        aws_s3_buckets.block_public_acls != true
+        and (
+            uri = 'http://acs.amazonaws.com/groups/global/AllUsers'
+            and permission in ('WRITE_ACP', 'FULL_CONTROL')
+        )
     )
     OR (
         aws_s3_buckets.block_public_policy != true
