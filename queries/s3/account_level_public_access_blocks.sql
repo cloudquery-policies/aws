c89d695a01c99cc65e49f92362f15c278cc9a68f
bbernays <ben@bernays.me>
2021-11-23 14:20:41 -0500

    Update account_level_public_access_blocks.sql

--- queries/s3/account_level_public_access_blocks.sql
+++ queries/s3/account_level_public_access_blocks.sql
@@ -4,6 +4,7 @@ from
     aws_accounts
 where
     account_id not in (
+        -- Find all accounts that do not have a s3 public block (either not configured or allows public access)
         select
             account_id
         from
