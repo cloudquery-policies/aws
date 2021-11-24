6b4a36922132646072035477bf65297d92b7da16
Yevgeny Pats <yev.pats@gmail.com>
2021-11-24 14:38:42 +0200

    use is distinct

--- queries/iam/password_policy_strong.sql
+++ queries/iam/password_policy_strong.sql
@@ -1,8 +1,8 @@
 SELECT account_id as resource_identifier FROM aws_iam_password_policies
     WHERE (
-        require_uppercase_characters = FALSE OR
-        require_lowercase_characters = FALSE OR
-        require_numbers = FALSE OR
+        require_uppercase_characters IS DISTINCT FROM TRUE OR
+        require_lowercase_characters IS DISTINCT FROM TRUE OR
+        require_numbers IS DISTINCT FROM TRUE OR
         minimum_password_length < 14 OR
         password_reuse_prevention is NULL OR
         max_password_age is NULL OR
