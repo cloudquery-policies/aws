72626a4f669dd881c1a1e990658ebef9e9d310ca
Andrii Romanenko <andrejjrom@gmail.com>
2021-11-26 11:29:11 +0200

    query adjusted

--- queries/ec2/check_default_sg_rules.sql
+++ queries/ec2/check_default_sg_rules.sql
@@ -9,21 +9,26 @@ WITH security_group_permissions AS (SELECT sg.account_id,
                                              JOIN aws_ec2_security_group_ip_permissions_egresses aesgipe ON
                                         sg.cq_id = aesgipe.security_group_cq_id
                                     WHERE sg.group_name = 'default'),
-     all_rules AS (SELECT p.account_id,
-                          p.arn,
-                          p.region,
-                          COUNT(*) AS rules
-                   FROM security_group_permissions p
-                            LEFT JOIN aws_ec2_security_group_ip_permission_ip_ranges in4
-                                      ON p.permission_id = in4.security_group_ip_permission_cq_id
-                            LEFT JOIN aws_ec2_security_group_ip_permission_ipv6_ranges in6
-                                      ON p.permission_id = in6.security_group_ip_permission_cq_id
-                            LEFT JOIN aws_ec2_security_group_ip_permissions_egress_ip_ranges out4
-                                      ON p.egress_permission_id = out4.security_group_ip_permissions_egress_cq_id
-                            LEFT JOIN aws_ec2_security_group_ip_permissions_egress_ipv6_ranges out6
-                                      ON p.egress_permission_id = out6.security_group_ip_permissions_egress_cq_id
-                   GROUP BY p.account_id, p.arn, p.region
-     )
-SELECT account_id, arn, region
-FROM all_rules
-WHERE rules > 0;
+     all_ranges AS (SELECT p.account_id,
+                           p.region,
+                           p.arn,
+                           (SELECT COUNT(*)
+                            FROM aws_ec2_security_group_ip_permission_ip_ranges
+                            WHERE security_group_ip_permission_cq_id = p.permission_id
+                           ) AS in_ips,
+                           (SELECT COUNT(*)
+                            FROM aws_ec2_security_group_ip_permission_ipv6_ranges aesgipir
+                            WHERE security_group_ip_permission_cq_id = p.permission_id
+                           ) AS in_ip6s,
+                           (SELECT COUNT(*)
+                            FROM aws_ec2_security_group_ip_permissions_egress_ip_ranges aesgipir
+                            WHERE security_group_ip_permissions_egress_cq_id = p.egress_permission_id
+                           ) AS out_ips,
+                           (SELECT COUNT(*)
+                            FROM aws_ec2_security_group_ip_permissions_egress_ipv6_ranges aesgipir
+                            WHERE security_group_ip_permissions_egress_cq_id = p.egress_permission_id
+                           ) AS out_ip6s
+                    FROM security_group_permissions p)
+SELECT account_id, region, arn
+FROM all_ranges
+WHERE in_ips + in_ip6s + out_ips + out_ip6s > 0;
