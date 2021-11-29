0dca968d539bd4deb1001a51c6d97701ee4feeb7
bbernays <ben@bernays.me>
2021-11-29 14:11:22 -0500

    Update restrict_cross_account_actions.sql

--- queries/s3/restrict_cross_account_actions.sql
+++ queries/s3/restrict_cross_account_actions.sql
@@ -5,11 +5,13 @@ SELECT ARN,
 FROM
 	(SELECT AWS_S3_BUCKETS.ARN,
 			ACCOUNT_ID,
+            -- For each Statement return an array containing the prinicipals
 			CASE
 							WHEN JSONB_TYPEOF(STATEMENTS -> 'Principal') = 'string' THEN JSONB_BUILD_ARRAY(STATEMENTS -> 'Principal')
 							WHEN JSONB_TYPEOF(STATEMENTS -> 'Principal' -> 'AWS') = 'string' THEN JSONB_BUILD_ARRAY(STATEMENTS -> 'Principal' -> 'AWS')
 							WHEN JSONB_TYPEOF(STATEMENTS -> 'Principal' -> 'AWS') = 'array' THEN STATEMENTS -> 'Principal' -> 'AWS'
 			END AS PRINCIPALS,
+            -- For each Statement return an array containing the Actions
 			CASE
 							WHEN JSONB_TYPEOF(STATEMENTS -> 'Action') = 'string' THEN JSONB_BUILD_ARRAY(STATEMENTS -> 'Action')
 							WHEN JSONB_TYPEOF(STATEMENTS -> 'Action') = 'array' THEN STATEMENTS -> 'Action'
@@ -19,9 +21,12 @@ FROM
 		WHERE STATEMENTS -> 'Effect' = '"Allow"' ) AS FOO,
 	JSONB_ARRAY_ELEMENTS(TO_JSONB(ACTIONS)) AS A,
 	JSONB_ARRAY_ELEMENTS(TO_JSONB(PRINCIPALS)) AS P
-WHERE (P.VALUE::text not like '"arn:aws:iam::' || ACCOUNT_ID || ':%"'
-							OR P.VALUE::text = '"*"')
-	AND (A.VALUE::text like '"s3:%*"'
-						OR A.VALUE::text like '"s3:DeleteObject"') 
+WHERE 
+    -- Any cross account prinicipals (or unknown principals) get flagged
+    (P.VALUE::text not like '"arn:aws:iam::' || ACCOUNT_ID || ':%"'OR P.VALUE::text = '"*"')
+	-- Any broad permissions or Deletes get flagged
+    AND (A.VALUE::text like '"s3:%*"' OR A.VALUE::text like '"s3:DeleteObject"') 
+
 -- This will flag ALL canoninical IDs as NOT COMPLIANT
 -- This will flag ALL users that have been deleted as NOT COMPLIANT
+-- This will not catch if an explicit deny supercedes the statement
