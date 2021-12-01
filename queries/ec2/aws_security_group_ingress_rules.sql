8714f3e69013688a21ff37656aa088bb2611f897
Andrii Romanenko <andrejjrom@gmail.com>
2021-12-01 20:18:42 +0200

    arn added to view

--- queries/ec2/aws_security_group_ingress_rules.sql
+++ queries/ec2/aws_security_group_ingress_rules.sql
@@ -1,6 +1,7 @@
 WITH sg_rules_ports AS (SELECT sg.account_id,
                                sg.region,
                                sg.group_name,
+                               sg.arn,
                                sg.id,
                                p.from_port,
                                p.to_port,
@@ -10,12 +11,11 @@ WITH sg_rules_ports AS (SELECT sg.account_id,
                                  LEFT JOIN aws_ec2_security_group_ip_permissions p
                                            ON sg.cq_id = p.security_group_cq_id)
 SELECT sgs.*, r.cidr_ip AS ip
-                      FROM sg_rules_ports sgs
-                               LEFT JOIN aws_ec2_security_group_ip_permission_ip_ranges r
-                                         ON sgs.permission_id = r.security_group_ip_permission_cq_id
-                      UNION
-                      SELECT sgs.*, r.cidr_ipv6 AS ip
-                      FROM sg_rules_ports sgs
-                               LEFT JOIN aws_ec2_security_group_ip_permission_ipv6_ranges r
-                                         ON sgs.permission_id = r.security_group_ip_permission_cq_id
-     
+FROM sg_rules_ports sgs
+         LEFT JOIN aws_ec2_security_group_ip_permission_ip_ranges r
+                   ON sgs.permission_id = r.security_group_ip_permission_cq_id
+UNION
+SELECT sgs.*, r.cidr_ipv6 AS ip
+FROM sg_rules_ports sgs
+         LEFT JOIN aws_ec2_security_group_ip_permission_ipv6_ranges r
+                   ON sgs.permission_id = r.security_group_ip_permission_cq_id
