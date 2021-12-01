83bf8cb56ceebe2f2d4130bc3a4c3e7f60a2d38f
Andrii Romanenko <andrejjrom@gmail.com>
2021-12-01 20:08:19 +0200

    review suggestions

--- queries/ec2/instances_with_more_than_2_network_interfaces.sql
+++ queries/ec2/instances_with_more_than_2_network_interfaces.sql
@@ -1,14 +1,10 @@
-WITH interfaces AS (SELECT account_id,
-                           region,
-                           id,
-                           COUNT(n.cq_id) AS interfaces
-                    FROM aws_ec2_instances i
-                             LEFT JOIN aws_ec2_instance_network_interfaces n ON i.cq_id = n.instance_cq_id
-                    GROUP BY account_id,
-                             region,
-                             id)
 SELECT account_id,
-       region,
-       id
-FROM interfaces
-WHERE interfaces > 1;
+    region,
+    id,
+    COUNT(n.cq_id) AS interfaces
+FROM aws_ec2_instances i
+LEFT JOIN aws_ec2_instance_network_interfaces n ON i.cq_id = n.instance_cq_id
+GROUP BY account_id,
+    region,
+    id
+HAVING COUNT(n.cq_id) > 1
