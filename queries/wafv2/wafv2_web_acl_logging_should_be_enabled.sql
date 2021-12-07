dcb1f4db26c879985da39dec2c2e6a99fc125ab5
Itay Zagron <zagron@gmail.com>
2021-12-07 15:47:41 +0100

    CR

--- queries/wafv2/wafv2_web_acl_logging_should_be_enabled.sql
+++ queries/wafv2/wafv2_web_acl_logging_should_be_enabled.sql
@@ -3,7 +3,7 @@ SELECT account_id,
        arn
 FROM aws_waf_web_acls
 WHERE logging_configuration IS NULL
-   OR logging_configuration = '{}';
+   OR logging_configuration = '{}'
 
 UNION
 -- WAF V2
