47094e8743728eb7361a7f609da5004d192f4d77
bbernays <ben@cloudquery.io>
2022-01-10 17:55:56 -0500

    Allow for Minor Version upgrades (#87)
    * Allow for minor updates
    
    * Update section_4.hcl

--- queries/lambda/lambda_function_prohibit_public_access.sql
+++ queries/lambda/lambda_function_prohibit_public_access.sql
@@ -2,14 +2,12 @@ SELECT account_id,
        region,
        arn
 FROM aws_lambda_functions,
-     jsonb_array_elements(policy_document -> 'Statement') AS statment
+    jsonb_array_elements(policy_document -> 'Statement') AS statment
 WHERE statment ->> 'Effect' = 'Allow'
-  AND (
-    statment->> 'Principal' = '*'
-   OR
-    statment-> 'Principal' ->> 'AWS' = '*'
-   OR
-    (statment -> 'Principal' ->> 'AWS')::jsonb ? '*'
-)
+    AND (
+        statment ->> 'Principal' = '*'
+        OR statment -> 'Principal' ->> 'AWS' = '*'
+        OR (statment -> 'Principal' ->> 'AWS')::JSONB ? '*'
+    )
 
 GROUP BY arn;
