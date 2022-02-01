a87f06bae3bb2f17e0a250ef14d445b8fcd67ef1
Andrii Romanenko <andrejjrom@gmail.com>
2022-02-01 22:07:48 +0200

    linter

--- queries/rds/rds_databases_and_clusters_should_not_use_a_database_engine_default_port.sql
+++ queries/rds/rds_databases_and_clusters_should_not_use_a_database_engine_default_port.sql
@@ -2,18 +2,17 @@
     SELECT arn
     FROM aws_rds_clusters
     WHERE (engine IN ('aurora', 'aurora-mysql', 'mysql') AND port = 3306)
-       OR (engine LIKE '%postgres%' AND port = '5432')
+                      OR (engine LIKE '%postgres%' AND port = '5432')
 )
 UNION
 (
     SELECT arn
     FROM aws_rds_instances
     WHERE (
-                engine IN (
-                           'aurora', 'aurora-mysql', 'mariadb', 'mysql'
-                ) AND instance_port = 3306
+        engine IN ( 'aurora', 'aurora-mysql', 'mariadb', 'mysql' )
+        AND instance_port = 3306
         )
-       OR (engine LIKE '%postgres%' AND instance_port = '5432')
-       OR (engine LIKE '%oracle%' AND instance_port = '1521')
-       OR (engine LIKE '%sqlserver%' AND instance_port = '1433')
+        OR (engine LIKE '%postgres%' AND instance_port = '5432')
+        OR (engine LIKE '%oracle%' AND instance_port = '1521')
+        OR (engine LIKE '%sqlserver%' AND instance_port = '1433')
 )
