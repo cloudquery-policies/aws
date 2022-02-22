c1705a1fe23be191dd1a59638c2c0321879b3593
bbernays <ben@bernays.me>
2022-02-22 13:37:18 -0500

    Update inline_policy_blocked_kms_actions.sql

--- queries/kms/inline_policy_blocked_kms_actions.sql
+++ queries/kms/inline_policy_blocked_kms_actions.sql
@@ -1,60 +1,60 @@
 SELECT arn,
        account_id
 FROM
-	(   
+    (
         -- select all user policies
         SELECT statement,
-			aws_iam_users.account_id,
-			arn,
-			aws_iam_users.cq_id
-		FROM aws_iam_user_policies
-		CROSS JOIN LATERAL jsonb_array_elements(
-            CASE JSONB_TYPEOF(policy_document -> 'Statement')
-                WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
-                WHEN 'array' THEN policy_document -> 'Statement' END
-        ) as statement
-		JOIN aws_iam_users ON aws_iam_users.cq_id = aws_iam_user_policies.user_cq_id
-		UNION 
+            aws_iam_users.account_id,
+            arn,
+            aws_iam_users.cq_id
+        FROM aws_iam_user_policies
+            CROSS JOIN LATERAL jsonb_array_elements(
+                CASE JSONB_TYPEOF(policy_document -> 'Statement')
+                    WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
+                    WHEN 'array' THEN policy_document -> 'Statement' END
+                ) AS statement
+            JOIN aws_iam_users ON aws_iam_users.cq_id = aws_iam_user_policies.user_cq_id
+        UNION
         -- select all role policies
         SELECT statement,
-			aws_iam_roles.account_id,
-			arn,
-			aws_iam_roles.cq_id
-		FROM aws_iam_role_policies
-	 	CROSS JOIN LATERAL jsonb_array_elements(
-            CASE JSONB_TYPEOF(policy_document -> 'Statement')
-                WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
-                WHEN 'array' THEN policy_document -> 'Statement' END
-        ) as statement
-		JOIN aws_iam_roles ON aws_iam_roles.cq_id = aws_iam_role_policies.role_cq_id
-		WHERE LOWER(arn) NOT LIKE 'arn:aws:iam::%:role/aws-service-role/%'
-		UNION 
+            aws_iam_roles.account_id,
+            arn,
+            aws_iam_roles.cq_id
+        FROM aws_iam_role_policies
+             CROSS JOIN LATERAL jsonb_array_elements(
+                CASE JSONB_TYPEOF(policy_document -> 'Statement')
+                    WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
+                    WHEN 'array' THEN policy_document -> 'Statement' END
+             ) AS statement
+            JOIN aws_iam_roles ON aws_iam_roles.cq_id = aws_iam_role_policies.role_cq_id
+        WHERE LOWER(arn) NOT LIKE 'arn:aws:iam::%:role/aws-service-role/%'
+        UNION
         -- select all group policies
         SELECT statement,
-			aws_iam_groups.account_id,
-			arn,
-			aws_iam_groups.cq_id
-		FROM aws_iam_group_policies
-	 	CROSS JOIN LATERAL jsonb_array_elements(
-            CASE JSONB_TYPEOF(policy_document -> 'Statement')
-                WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
-                WHEN 'array' THEN policy_document -> 'Statement' END
-        ) as statement
-		JOIN aws_iam_groups ON aws_iam_groups.cq_id = aws_iam_group_policies.group_cq_id) T
-WHERE 
-	statement ->> 'Effect' = 'Allow'
-	AND LOWER(statement::text)::JSONB -> 'resource' ?| array[
-		'*',
-		'arn:aws:kms:*:*:key/*',
-		'arn:aws:kms:*:' || account_id || ':key/*'
-		'arn:aws:kms:*:*:alias/*',
-		'arn:aws:kms:*:' || account_id || ':alias/*'
+            aws_iam_groups.account_id,
+            arn,
+            aws_iam_groups.cq_id
+        FROM aws_iam_group_policies
+            CROSS JOIN LATERAL jsonb_array_elements(
+                CASE JSONB_TYPEOF(policy_document -> 'Statement')
+                    WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
+                    WHEN 'array' THEN policy_document -> 'Statement' END
+                ) AS statement
+            JOIN aws_iam_groups ON aws_iam_groups.cq_id = aws_iam_group_policies.group_cq_id) T
+WHERE
+    statement ->> 'Effect' = 'Allow'
+    AND LOWER(statement::TEXT)::JSONB -> 'resource' ?| ARRAY[
+        '*',
+        'arn:aws:kms:*:*:key/*',
+        'arn:aws:kms:*:' || account_id || ':key/*'
+        'arn:aws:kms:*:*:alias/*',
+        'arn:aws:kms:*:' || account_id || ':alias/*'
 	] --noqa
 
-	AND LOWER(statement::text)::JSONB -> 'action' ?| array[
-		'*',
-		'kms:*',
-		'kms:decrypt',
-		'kms:encrypt*',
-		'kms:reencryptfrom'
-	]
+    AND LOWER(statement::TEXT)::JSONB -> 'action' ?| ARRAY[
+        '*',
+        'kms:*',
+        'kms:decrypt',
+        'kms:encrypt*',
+        'kms:reencryptfrom'
+    ]
