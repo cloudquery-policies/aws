5e0fd730dab528de9181019e159fbffdb8982887
bbernays <ben@bernays.me>
2022-02-23 08:21:48 -0500

    Update lambda_function_prohibit_public_access.sql

--- queries/lambda/lambda_function_prohibit_public_access.sql
+++ queries/lambda/lambda_function_prohibit_public_access.sql
@@ -2,7 +2,12 @@ SELECT account_id,
        region,
        arn
 FROM aws_lambda_functions,
-    jsonb_array_elements(policy_document -> 'Statement') AS statment
+    jsonb_array_elements(
+            CASE JSONB_TYPEOF(policy_document -> 'Statement')
+                WHEN 'string' THEN JSONB_BUILD_ARRAY(policy_document ->> 'Statement')
+                WHEN 'array' THEN policy_document -> 'Statement'
+            END
+        ) AS statement
 WHERE statment ->> 'Effect' = 'Allow'
     AND (
         statment ->> 'Principal' = '*'
