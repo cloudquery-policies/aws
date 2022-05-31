6bbe51a656a0e076aad96c3a7f32ed6987489bff
bbernays <ben@bernays.me>
2022-05-31 11:22:21 -0400

    fix syntax

--- queries/secretsmanager/secrets_should_be_rotated_within_a_specified_number_of_days.sql
+++ queries/secretsmanager/secrets_should_be_rotated_within_a_specified_number_of_days.sql
@@ -2,7 +2,7 @@ SELECT account_id,
        region,
        arn,
        name,
-       format('secret was last rotated %s days', COALESCE(DATE_PART('days', NOW() - last_rotated_date), DATE_PART('days', NOW() - created_date)) as cq_reason
+       format('secret was last rotated %s days', COALESCE(DATE_PART('days', NOW() - last_rotated_date), DATE_PART('days', NOW() - created_date))) as cq_reason
 FROM aws_secretsmanager_secrets
 WHERE (last_rotated_date IS NULL AND created_date > NOW() - INTERVAL '90 days')
        OR (last_rotated_date IS NOT NULL AND last_rotated_date > NOW() - INTERVAL '90 days');
