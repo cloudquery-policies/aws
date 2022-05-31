593f7bd3d86b7c5aa0716ee3a29a12a11807edb5
bbernays <ben@bernays.me>
2022-05-31 11:48:16 -0400

    syntax

--- queries/ec2/instances_with_more_than_2_network_interfaces.sql
+++ queries/ec2/instances_with_more_than_2_network_interfaces.sql
@@ -2,7 +2,7 @@ SELECT account_id,
     region,
     id,
     COUNT(n.cq_id) AS interfaces,
-    format('remove %d interfaces from instance',COUNT(n.cq_id)-1) as cq_reason
+    format('remove %s interfaces from instance',COUNT(n.cq_id)-1) as cq_reason
 FROM aws_ec2_instances i
     LEFT JOIN
         aws_ec2_instance_network_interfaces n ON i.cq_id = n.instance_cq_id
