f04bde1116b556e5eb458d2b81257fb77e9f6f79
bbernays <ben@bernays.me>
2022-05-31 11:39:33 -0400

    fix es query syntax

--- queries/elasticsearch/elasticsearch_domain_error_logging_to_cloudwatch_logs_should_be_enabled.sql
+++ queries/elasticsearch/elasticsearch_domain_error_logging_to_cloudwatch_logs_should_be_enabled.sql
@@ -3,9 +3,9 @@ SELECT
     region,
     arn,
     CASE
-        WHEN  log_publishing_options -> 'ES_APPLICATION_LOGS' -> 'Enabled' IS DISTINCT FROM 'true' THEN 'ES application logs should be enabled',
+        WHEN  log_publishing_options -> 'ES_APPLICATION_LOGS' -> 'Enabled' IS DISTINCT FROM 'true' THEN 'ES application logs should be enabled'
         WHEN  log_publishing_options -> 'ES_APPLICATION_LOGS' -> 'CloudWatchLogsLogGroupArn' IS NULL THEN 'logs should be sent to Cloudwatch'
-    END as 'cq_reason'
+    END as cq_reason
 FROM aws_elasticsearch_domains
 WHERE
     log_publishing_options -> 'ES_APPLICATION_LOGS' -> 'Enabled' IS DISTINCT FROM 'true'
--- queries/elasticsearch/elasticsearch_domains_should_be_configured_with_at_least_three_dedicated_master_nodes.sql
+++ queries/elasticsearch/elasticsearch_domains_should_be_configured_with_at_least_three_dedicated_master_nodes.sql
@@ -1,9 +1,9 @@
 SELECT account_id, region, arn
 CASE
-    WHEN  cluster_dedicated_master_enabled IS NOT TRUE THEN 'cluster should have dedicated master node enabled',
-    WHEN  cluster_dedicated_master_count IS NULL THEN 'cluster should have at least 3 master nodes',
+    WHEN  cluster_dedicated_master_enabled IS NOT TRUE THEN 'cluster should have dedicated master node enabled'
+    WHEN  cluster_dedicated_master_count IS NULL THEN 'cluster should have at least 3 master nodes'
     WHEN  cluster_dedicated_master_count < 3 THEN 'cluster should have at least 3 master nodes'
-END as 'cq_reason'
+END as cq_reason
 
 FROM aws_elasticsearch_domains
 WHERE
