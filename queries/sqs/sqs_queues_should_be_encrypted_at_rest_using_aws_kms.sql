2dcdfc5c48311e6ecfe97526e61b4ab216d945ac
bbernays <ben@bernays.me>
2022-05-31 11:48:27 -0400

    syntax

--- queries/sqs/sqs_queues_should_be_encrypted_at_rest_using_aws_kms.sql
+++ queries/sqs/sqs_queues_should_be_encrypted_at_rest_using_aws_kms.sql
@@ -1,7 +1,7 @@
 SELECT account_id,
        region,
        arn,
-       "SNS queues should be encrypted at rest using kms" as cq_reason
+       'SNS queues should be encrypted at rest using kms' as cq_reason
 FROM aws_sqs_queues
 WHERE kms_master_key_id IS NULL
       OR kms_master_key_id = '';
